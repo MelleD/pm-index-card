2a487f7542aa95da2e8ef62d02f639eb9d49653d
Dennis Melzer <melzer.dennis@gmail.com>
2024-05-30 20:49:13 +0200

    Fix rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,21 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from "@rollup/plugin-typescript";
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import serve from 'rollup-plugin-serve';
 import json from '@rollup/plugin-json';
+import ignore from "./rollup-plugins/ignore.js";
+
+const IGNORED_FILES = [
+    "@material/mwc-notched-outline/mwc-notched-outline.js",
+    "@material/mwc-ripple/mwc-ripple.js",
+    "@material/mwc-list/mwc-list.js",
+    "@material/mwc-list/mwc-list-item.js",
+    "@material/mwc-menu/mwc-menu.js",
+    "@material/mwc-menu/mwc-menu-surface.js",
+    "@material/mwc-icon/mwc-icon.js",
+];
 
 
 const dev = process.env.ROLLUP_WATCH;
@@ -22,22 +33,36 @@ const serveopts = {
 const plugins = [
   nodeResolve({}),
   commonjs(),
-  typescript(),
+  typescript({
+    declaration: false,
+  }),
   json(),
   babel({
-    exclude: 'node_modules/**',
+    babelHelpers: "bundled",
   }),
-  dev && serve(serveopts),
-  !dev && terser()
+  ignore({
+    files: IGNORED_FILES.map((file) => require.resolve(file)),
+  }),
+  ...(dev ? [serve(serveopts)] : [terser()]),
 ];
 
 export default [
   {
-    input: 'src/pm-index-card.ts',
-    output: {
-      dir: 'dist',
-      format: 'es',
-    },
-    plugins: [...plugins],
+      input: "src/pm-index-card.ts",
+      output: {
+          dir: "dist",
+          format: "es",
+          inlineDynamicImports: true,
+      },
+      plugins,
+      moduleContext: (id) => {
+          const thisAsWindowForModules = [
+              "node_modules/@formatjs/intl-utils/lib/src/diff.js",
+              "node_modules/@formatjs/intl-utils/lib/src/resolve-locale.js",
+          ];
+          if (thisAsWindowForModules.some((id_) => id.trimRight().endsWith(id_))) {
+              return "window";
+          }
+      },
   },
 ];
